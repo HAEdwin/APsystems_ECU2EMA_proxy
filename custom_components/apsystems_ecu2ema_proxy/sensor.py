54f7f0a7e51461ac5e5b3b636e3de88522c41a03
HAEdwin <82239374+HAEdwin@users.noreply.github.com>
2023-03-03 08:29:54 +0100

    Update sensor.py

--- custom_components/apsystems_ecu2ema_proxy/sensor.py
+++ custom_components/apsystems_ecu2ema_proxy/sensor.py
@@ -6,7 +6,8 @@ from homeassistant.components.sensor import (
     SensorEntity,
     SensorStateClass,
 )
-from homeassistant.const import TEMP_CELSIUS
+#from homeassistant.const import TEMP_CELSIUS
+from homeassistant.const import POWER_WATT
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
@@ -25,38 +26,30 @@ host = '172.16.0.19' #Your host IP-address here
 class PROXYSERVER(BaseRequestHandler):
     def handle(self):
         (myhost, myport) = self.server.server_address
-        
+        global ecu_current_power
         rec = self.request.recv(1024)
         if rec:
-            LOGGER.warning(f"From ECU @{self.client_address[0]}:{myport} - {rec}")
+            logging.warning(f"From ECU @{self.client_address[0]}:{myport} - {rec}")
+
+            # Data to sensors
+            if (rec[0:7].decode('ASCII')) == "APS18AA":
+                ecu_current_power = int(rec[30:42])/100
 
             # forward message to EMA
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect(("3.67.1.32", myport))
-            try:
-                sock.sendall(rec)
-                #LOGGER.warning(f"Forwarded to @EMA:{myport} - {rec}")
-                response = sock.recv(1024)
-                LOGGER.warning(f"From EMA: {response}\n")
-            finally:
-                sock.close()
-            
+            sock.connect(("3.67.1.32", myport)) # don't use ecu.apsystemsema.com due to rewrite it will loop
+            sock.sendall(rec)
+
+            # receive message from EMA    
+            response = sock.recv(1024)
+            logging.warning(f"From EMA: {response}\n")
+            sock.close()
+
             # return message to ECU
-            #LOGGER.warning(f"Sending back to ECU: {response}\n")
             self.request.send(response)
-
-
-# Dit is de client voor de connectie naar de EMA server met variabele poort
-# aangeroepen met: client(ip, PORT, b'Hello World 1')
-def client(ip, port, message):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((ip, port))
-    try:
-        sock.sendall(message)
-        response = sock.recv(1024)
-        LOGGER.warning(f"Received: {response}")
-    finally:
-        sock.close()
+            # Update local Sensor
+            EMASensor.update(response)
+            
 
 def setup_platform(
     hass: HomeAssistant,
@@ -67,6 +60,7 @@ def setup_platform(
     """Set up the sensor platform."""
     add_entities([EMASensor()])
 
+    """Setup the listeners and threads."""
     listener_1 = socketserver.TCPServer((host, 8995), PROXYSERVER)
     thread_1 = threading.Thread(target=listener_1.serve_forever)
     listener_2 = socketserver.TCPServer((host, 8996), PROXYSERVER)
@@ -79,12 +73,12 @@ def setup_platform(
 
 class EMASensor(SensorEntity):
     """Representation of a Sensor."""
-    _attr_name = "Example Temperature"
-    _attr_native_unit_of_measurement = TEMP_CELSIUS
-    _attr_device_class = SensorDeviceClass.TEMPERATURE
+    _attr_name = "ECU Current Power"
+    _attr_native_unit_of_measurement = POWER_WATT
+    _attr_device_class = SensorDeviceClass.POWER
     _attr_state_class = SensorStateClass.MEASUREMENT
 
     def update(self) -> None:
         # Fetch new state data for the sensor, this is the only method that should fetch new data for Home Assistant.
         #LOGGER.warning("Sensor update...")
-        self._attr_native_value = 23
+        self._attr_native_value = ecu_current_power
